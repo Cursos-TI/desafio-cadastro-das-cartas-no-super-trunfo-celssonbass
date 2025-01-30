673574896b5501ec615081fc4793fc242ce9466d
celssonbass <42822782+celssonbass@users.noreply.github.com>
2025-01-30 00:21:20 +0000

    acrescentado a densidade populacional e pib per capta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,8 @@ int main() {
     float area; // VARIAVEL QUE ARMAZENA A ÁREA
     float pib; // VARIAVEL QUE ARMAZENA O PIB
     int pontosTuristicos;// VARIAVEL QUE ARMAZENA OS PONTOS TURISTICOS
+    float densidadePopulacional; // VARIAVEL QUE ARMAZENA A DENSIDADE POPULACIONAL
+    float pibPerCapta; // VARIAVEL QUE ARMAZENA O PIB PER CAPITA
 
     // EXIBE UMA MENSAGEM DE BOAS VINDAS AO JOGO
     printf("Olá, seja bem vindo ao Super Trunfo em C\n");
@@ -26,21 +28,25 @@ int main() {
 
     // EXIBE A MENSAGEM PARA O USUARIO DIGITAR O ESTADO
     printf("Por favor, digite o estado\n");
+
     // FUNCAO QUE VAI RECEBER O ESTADO E ARMAZENAR NA VARIAVEL estado
     scanf("%c", &estado);
 
     // EXIBE A MENSAGEM PARA O USUARIO DIGITAR O CÓDIGO DA CARTA
     printf("Digite o código da carta\n");
+    
     // FUNCAO QUE VAI RECEBER O CÓDIGO E ARMAZENAR NA VARIAVEL codigoCarta
     scanf("%s", &codigoDaCarta);
 
     // EXIBE A MENSAGEM PARA O USUARIO DIGITAR O NOME DA CIDADE
     printf("Digite o nome da cidade\n");
+
     // FUNCAO QUE VAI RECEBER O NOME DA CIDADE ARMAZENAR NA VARIAVEL nomeCidade
     scanf("%s", &nomeDaCidade);
 
     // EXIBE A MENSAGEM PARA O USUARIO DIGITAR A POPULAÇÃO
     printf("Digite a população\n");
+
     // FUNCAO QUE VAI RECEBER POPULAÇÃO ARMAZENAR NA VARIAVEL populacao
     scanf("%d", &populacao);
 
@@ -56,8 +62,15 @@ int main() {
 
     // EXIBE A MENSAGEM PARA O USUARIO DIGITAR o NÚMERO DE PONTOS TURISTICOS
     printf("Digite o numero de pontos turisticos\n");
+
     // FUNCAO QUE VAI RECEBER O NUMERO DE PONTOS TURISTICOS E ARMAZENAR NA VARIAVEL pontosTuristicos
     scanf("%d", &pontosTuristicos);
+
+    //FUNÇÃO PARA CALCULAR A DENSIDADE POPULACIONAL
+    densidadePopulacional = (float) populacao / area;
+
+    //FUNÇÃO PARA CALCULAR O PIB PER CAPTA
+    pibPerCapta = (float) pib / populacao;
 
     //DEFINE UMA NOVA LINHA EM BRANCO
     printf("\n");
@@ -69,10 +82,12 @@ int main() {
     printf("Estado: %c\n", estado); // EXIBE O ESTADO CADASTRADO
     printf("Código da Carta: %s\n", codigoDaCarta); // EXIBE O CÓDIGO DA CARTA
     printf("Nome da Cidade: %s\n", nomeDaCidade); // EXIBE O NOME DA CIDADE
-    printf("População: %d\n", populacao); // EXIBE A POPULAÇÃO
+    printf("População: %d habitantes\n", populacao); // EXIBE A POPULAÇÃO
     printf("Área: %f Km²\n", area); // EXIBE A ÁREA
     printf("PIB: %f bilhões de reais\n", pib); // EXIBE O PIB
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos); // EXIBE O NUMERO DE PONTOS TURISTICOS
+    printf("Densidade Populacional: %f\n", densidadePopulacional); // EXIBE DENSIDADE POPULACIONAL
+    printf("PIB Per Capita: %f reais\n", pibPerCapta); // EXIBE O PIB PER CAPTA
 
     return 0;
 }
